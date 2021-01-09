62f8742c7febe47875a9285ff6c44f7017886f8c
ferdiard <77181335+ferdiard@users.noreply.github.com>
2021-01-09 11:18:54 +0700

    FERDBOT index.js

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const menu = require("./lib/menu.js");
 const donate = require("./lib/donate.js");
 const info = require("./lib/info.js");
 //
-const BotName = 'ST4RZ BOT 🤖'; // Nama Bot Whatsapp
-const instagramlu = 'https://instagram.com/bintang_nur_pradana'; // Nama Instagramlu cok
-const whatsapplu = '0819-4698-3575'; // Nomor whatsapplu cok
+const BotName = 'FERDBot 🤖'; // Nama Bot Whatsapp
+const instagramlu = 'https://instagram.com/ferdi_ardian21'; // Nama Instagramlu cok
+const whatsapplu = '0821-6936-9877'; // Nomor whatsapplu cok
 const kapanbotaktif = '24 Jam'; // Kapan bot lu aktif
-const grupch1 = 'https://chat.whatsapp.com/FsAlnxqz6y2BhCQi5ayCLG'; // OFFICIAL GRUP LU 1
-const grupch2 = 'https://chat.whatsapp.com/KLW3UlFfeaH36Ucm5zRfCz'; // OFFICIAL GRUP LU 2
+const grupch1 = 'https://chat.whatsapp.com/GQRkuhH34C80Y6JWwG3xXl'; // OFFICIAL GRUP LU 1
+const grupch2 = 'https://chat.whatsapp.com/GQRkuhH34C80Y6JWwG3xXl'; // OFFICIAL GRUP LU 2
 //
 const
 {
